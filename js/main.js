f9b3654dc16be1c8dc88259c26ec1980d2da84a7
altrae <altrae@users.noreply.github.com>
2019-09-22 11:36:34 -0500

    converting main to vanilla js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,68 @@
-$(function() {
-    var imgs = [
-        "1.jpg",
-        "2.jpg",
-        "3.jpg"
-    ];
-    createImages(imgs);
+document.addEventListener('DOMContentLoaded', () => {
+    const images = [
+            'assets/images/1.jpg',
+            'assets/images/2.jpg',
+            'assets/images/3.jpg'
+        ],
+        navItems = document.querySelectorAll('nav li');
+
+    createImages(images);
     setInterval(rotateImages, 6000);
-	var navItem = $("nav ul li");
-	$(navItem).hover(highlight, highlight);
-	$(navItem).click(setActive);
+
+    navItems.forEach((navItem, index) => {
+        navItem.addEventListener('mouseenter', highlight);
+        navItem.addEventListener('mouseleave', highlight);
+        navItem.addEventListener('click', setActive);
+    });
 });
 
-function highlight(evt) {
-	$(this).toggleClass("highlighted");
+function highlight(event) {
+    const classes = event.currentTarget.classList
+
+    classes.contains('highlighted') ?
+        classes.remove('highlighted') :
+        classes.add('highlighted');
 }
 
-function setActive(evt) {
-	evt.preventDefault();
-	var navItem = $("nav ul li").not("active");
-	if(navItem) {
-		$(this).addClass("active");
-		$(this).siblings().removeClass("active");
-	}
-}
+function setActive(event) {
+    let element = event.currentTarget,
+        navItems = element.parentNode.querySelectorAll('nav li');
 
-function createImages(imgs) {
-    $("#hero").empty();
-    $.each(imgs, function(index, value) {
-        var img = $("<img src='" + value + "' />");
-        $("#hero").append(img);
+    event.preventDefault();
+
+    navItems.forEach((navItem, index) => {
+        navItem.classList.remove('active');
     });
-    $("#hero img:first").addClass("current");
+
+    element.classList.add('active');
+}
+
+function createImages(images) {
+    const hero = document.querySelector('#hero');
+
+    // Empty hero
+    while (hero.firstChild) {
+        hero.removeChild(hero.firstChild);
+    }
+
+    images.forEach((image, index) => {
+        const imgElement = document.createElement('img');
+
+        imgElement.src = image;
+        hero.append(imgElement);
+    });
+
+    document.querySelector('#hero img:first-child').classList.add('current');
 }
 
 function rotateImages() {
-    var curImg = $("#hero .current");
-    var nxtImg = curImg.next();
-    if(nxtImg.length == 0) { nxtImg = $("#hero img:first"); }
-    curImg.addClass("previous").removeClass("current");
-    nxtImg.addClass("current").css({opacity:"0"}).animate({opacity:"1"},3000,function() { curImg.removeClass("previous"); });
+    let currentImage = document.querySelector('#hero .current'),
+        nextImage = currentImage.nextSibling;
+
+    if (nextImage === null) {
+        nextImage = document.querySelector('#hero img:first-child');
+    }
+
+    currentImage.classList.remove('current');
+    nextImage.classList.add('current');
 }
