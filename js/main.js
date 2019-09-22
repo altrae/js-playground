63ae6a14d41133e0f75a8f2a50b1480ed9a05024
altrae <altrae@users.noreply.github.com>
2019-09-22 12:22:53 -0500

    refactor to use es6 class for slideshow

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
+class Slideshow {
+    constructor(images, interval = 3000) {
+        this.images = images;
+        this.interval = interval;
+    }
+
+    createImages() {
+        const hero = document.querySelector('#hero');
+
+        // Empty hero
+        while (hero.firstChild) {
+            hero.removeChild(hero.firstChild);
+        }
+
+        this.images.forEach((image, index) => {
+            const imgElement = document.createElement('img');
+
+            imgElement.src = image;
+            hero.append(imgElement);
+        });
+
+        document.querySelector('#hero img:first-child').classList.add('current');
+    }
+
+    rotateImages() {
+        let currentImage = document.querySelector('#hero .current'),
+            nextImage = currentImage.nextSibling;
+
+        if (nextImage === null) {
+            nextImage = document.querySelector('#hero img:first-child');
+        }
+
+        currentImage.classList.remove('current');
+        nextImage.classList.add('current');
+    }
+
+    init() {
+        this.createImages();
+        setInterval(this.rotateImages, this.interval);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const images = [
-            'assets/images/1.jpg',
-            'assets/images/2.jpg',
-            'assets/images/3.jpg'
-        ],
-        navItems = document.querySelectorAll('nav li');
+        'assets/images/1.jpg',
+        'assets/images/2.jpg',
+        'assets/images/3.jpg'
+    ];
 
-    createImages(images);
-    setInterval(rotateImages, 6000);
+    const slideshow = new Slideshow(images, 6000);
+
+    slideshow.init();
+
+    const navItems = document.querySelectorAll('nav li');
 
     navItems.forEach((navItem, index) => {
         navItem.addEventListener('mouseenter', highlight);
@@ -16,16 +60,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-function highlight(event) {
-    const classes = event.currentTarget.classList
+const highlight = event => {
+    const classes = event.currentTarget.classList;
 
-    classes.contains('highlighted') ?
-        classes.remove('highlighted') :
-        classes.add('highlighted');
-}
+    classes.toggle('highlighted');
+};
 
-function setActive(event) {
-    let element = event.currentTarget,
+const setActive = event => {
+    const element = event.currentTarget,
         navItems = element.parentNode.querySelectorAll('nav li');
 
     event.preventDefault();
@@ -35,34 +77,4 @@ function setActive(event) {
     });
 
     element.classList.add('active');
-}
-
-function createImages(images) {
-    const hero = document.querySelector('#hero');
-
-    // Empty hero
-    while (hero.firstChild) {
-        hero.removeChild(hero.firstChild);
-    }
-
-    images.forEach((image, index) => {
-        const imgElement = document.createElement('img');
-
-        imgElement.src = image;
-        hero.append(imgElement);
-    });
-
-    document.querySelector('#hero img:first-child').classList.add('current');
-}
-
-function rotateImages() {
-    let currentImage = document.querySelector('#hero .current'),
-        nextImage = currentImage.nextSibling;
-
-    if (nextImage === null) {
-        nextImage = document.querySelector('#hero img:first-child');
-    }
-
-    currentImage.classList.remove('current');
-    nextImage.classList.add('current');
-}
+};
