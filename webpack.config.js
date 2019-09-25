f2129101f532e4ad2aa8931f43df49d3ae507b8f
altrae <altrae@users.noreply.github.com>
2019-09-24 20:31:03 -0500

    adding sass-node to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/main.js',
-    mode: 'development',
+    entry: ['./js/main.js'],
+    // entry: ['./js/main.js', './css/main.scss'],
+    // module: {
+    //     rules: [{
+    //         test: /\.s[ac]ss$/i,
+    //         use: [
+    //             // Creates `style` nodes from JS strings
+    //             'style-loader',
+    //             // Translates CSS into CommonJS
+    //             'css-loader',
+    //             // Compiles Sass to CSS
+    //             'sass-loader',
+    //         ],
+    //     }, ],
+    // },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    // resolve: {
+    //     extensions: ['.js', '.jsx', '.css']
+    // }
 };
